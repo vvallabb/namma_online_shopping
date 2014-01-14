085171a68890da5d35ab5d3cbdfe4541828c79a6
Vasanth Vallabh <vvallabb@gmail.com>
2014-01-14 05:44:54 +0530

    tidying changes to shoppingcart
    removing unnecessary comments

--- Amazon/src/entity/ShoppingCart.java
+++ Amazon/src/entity/ShoppingCart.java
@@ -1,14 +1,6 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package entity;
-
-/**
- *
- * @author vasanth
- */
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -16,16 +8,16 @@ import dao.*;
 
 public class ShoppingCart {
 
-    private int ItemId;
-    private String ItemName;
-    private Date ItemDate;
-    private int ItemPrice;
-    private int ItemQuantity;
-    private float ItemDiscount;
-    private int ItemStatus;
-    private String itemType;
+	private int ItemId;
+	private String ItemName;
+	private Date ItemDate;
+	private int ItemPrice;
+	private int ItemQuantity;
+	private float ItemDiscount;
+	private int ItemStatus;
+	private String itemType;
 
-    public String getItemType() {
+	public String getItemType() {
 		return itemType;
 	}
 
@@ -33,137 +25,104 @@ public class ShoppingCart {
 		this.itemType = itemType;
 	}
 
+	public int getItemId() {
+		return ItemId;
+	}
+
+	public void setItemId(int ItemId) {
+		this.ItemId = ItemId;
+	}
+
+	public String getItemName() {
+		return ItemName;
+	}
+
+	public void setItemName(String ItemName) {
+		this.ItemName = ItemName;
+	}
+
+	public Date getItemDate() {
+		return ItemDate;
+	}
+
+
+	public void setItemDate(Date ItemDate) {
+		this.ItemDate = ItemDate;
+	}
+
+	public int getItemPrice() {
+		return ItemPrice;
+	}
+
 	/**
-     * @return the ItemId
-     */
-    public int getItemId() {
-        return ItemId;
-    }
+	 * @param ItemPrice
+	 *            the ItemPrice to set
+	 */
+	public void setItemPrice(int ItemPrice) {
+		this.ItemPrice = Math.round(ItemPrice);
+	}
 
-    /**
-     * @param ItemId the ItemId to set
-     */
-    public void setItemId(int ItemId) {
-        this.ItemId = ItemId;
-    }
+	public int getItemQuantity() {
+		return ItemQuantity;
+	}
 
-    /**
-     * @return the ItemName
-     */
-    public String getItemName() {
-        return ItemName;
-    }
+	public void setItemQuantity(int ItemQuantity) {
+		this.ItemQuantity = ItemQuantity;
+	}
 
-    /**
-     * @param ItemName the ItemName to set
-     */
-    public void setItemName(String ItemName) {
-        this.ItemName = ItemName;
-    }
+	public float getItemDiscount() {
+		return ItemDiscount;
+	}
 
-    /**
-     * @return the ItemDate
-     */
-    public Date getItemDate() {
-        return ItemDate;
-    }
+	public void setItemDiscount(float ItemDiscount) {
+		this.ItemDiscount = ItemDiscount;
+	}
 
-    /**
-     * @param ItemDate the ItemDate to set
-     */
-    public void setItemDate(Date ItemDate) {
-        this.ItemDate = ItemDate;
-    }
+	public ArrayList RetrieveShoppingCartDtls(String username) {
+		ShoppingCartDAO scDao = new ShoppingCartDAO();
+		ArrayList sc = scDao.getShoppingCartDetails(username);
+		if (sc == null)
+			System.out.println("null in scart.java");
+		return sc;
+	}
 
-    /**
-     * @return the ItemPrice
-     */
-    public int getItemPrice() {
-        return ItemPrice;
-    }
+	public boolean UpdateShoppingCartSaveForLater(int itemId, int status,
+			String username) {
+		ShoppingCartDAO scDao = new ShoppingCartDAO();
+		if (scDao.UpdateShoppingCartSaveForLaterDao(itemId, status, username))
+			return true;
+		else
+			return false;
+	}
 
-    /**
-     * @param ItemPrice the ItemPrice to set
-     */
-    public void setItemPrice(int ItemPrice) {
-        this.ItemPrice = Math.round(ItemPrice);
-    }
+	public boolean DeleteShoppingCartItem(int itemId, String username) {
+		ShoppingCartDAO scDao = new ShoppingCartDAO();
+		if (scDao.DeleteShoppingCartItemDao(itemId, username))
+			return true;
+		else
+			return false;
+	}
 
-    /**
-     * @return the ItemQuantity
-     */
-    public int getItemQuantity() {
-        return ItemQuantity;
-    }
+	public int getItemStatus() {
+		return ItemStatus;
+	}
 
-    /**
-     * @param ItemQuantity the ItemQuantity to set
-     */
-    public void setItemQuantity(int ItemQuantity) {
-        this.ItemQuantity = ItemQuantity;
-    }
 
-    /**
-     * @return the ItemDiscount
-     */
-    public float getItemDiscount() {
-        return ItemDiscount;
-    }
+	public void setItemStatus(int ItemStatus) {
+		this.ItemStatus = ItemStatus;
+	}
 
-    /**
-     * @param ItemDiscount the ItemDiscount to set
-     */
-    public void setItemDiscount(float ItemDiscount) {
-        this.ItemDiscount = ItemDiscount;
-    }
+	public boolean InsertShoppingCartDetails(String BookName,
+			String AuthorBookName, float BookPrice, int quantity,
+			String username, String itemType) {
+		ShoppingCartDAO scDao = new ShoppingCartDAO();
 
-public ArrayList RetrieveShoppingCartDtls(String username)
-{
-    ShoppingCartDAO scDao = new ShoppingCartDAO();
-    ArrayList sc = scDao.getShoppingCartDetails(username);
-    if(sc==null)
-    	System.out.println("null in scart.java");
-    return  sc;
-}
+		if (scDao.InsertShoppingCartItemDao(BookName, AuthorBookName,
+				BookPrice, quantity, username, itemType)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-public boolean UpdateShoppingCartSaveForLater(int itemId,int status,String username)
-{
-    ShoppingCartDAO scDao = new ShoppingCartDAO();
-    if(scDao.UpdateShoppingCartSaveForLaterDao(itemId,status,username))
-        return true;
-    else
-        return false;
-}
-
-public boolean DeleteShoppingCartItem(int itemId,String username)
-{
-    ShoppingCartDAO scDao = new ShoppingCartDAO();
-    if(scDao.DeleteShoppingCartItemDao(itemId,username))
-        return true;
-    else
-        return false;
-}
-    /**
-     * @return the ItemStatus
-     */
-    public int getItemStatus() {
-        return ItemStatus;
-    }
-
-    /**
-     * @param ItemStatus the ItemStatus to set
-     */
-    public void setItemStatus(int ItemStatus) {
-        this.ItemStatus = ItemStatus;
-    }
-//Vasanth code changes : Start
-    public boolean InsertShoppingCartDetails(String BookName, String AuthorBookName,   float BookPrice, int quantity,String username,String itemType){
-        ShoppingCartDAO scDao = new ShoppingCartDAO();
-
-        if(scDao.InsertShoppingCartItemDao (BookName, AuthorBookName, BookPrice,quantity,username,itemType) )
-            return true;
-        else
-            return false;
-    }
-//Vasanth code changes : End
 }
